6a8acf00f80c3785bab513a90a24a0ddff3bbccf
kwanghoon.kim <kk03187693@gmail.com>
2021-02-07 22:17:57 +0900

    add queue with java

---
+++ src/week3/MyQueue.java
@@ -0,0 +1,57 @@
+package week3;
+
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> {
+    class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node<T> first;
+    private Node<T> last;
+
+    public void add(T item) {
+        Node<T> addNode = new Node<T>(item);
+
+        if (last != null) {
+            last.next = addNode;
+        }
+        last = addNode;
+        if (first == null) {
+            first = last;
+        }
+    }
+
+    public T remove() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+
+        T data = first.data;
+        first = first.next;
+
+        if (first == null) {
+            last = null;
+        }
+        return data;
+    }
+
+    public T peek() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        if (first == null) {
+            return true;
+        }
+        return false;
+    }
+}
